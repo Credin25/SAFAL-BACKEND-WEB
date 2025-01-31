09d8330c22261dbd4241a9d3a5f483ee333371e2
Abrar <abrarkhira.int@credin.in>
2025-01-31 18:15:00 +0530

    changed cookies config

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import MainHome from './RoutePage/MainHome';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 axios.defaults.withCredentials = true; 
 function App() {
+  const authHeader = Cookies.get("accessToken")
+  
+  axios.interceptors.request.use((config) => {
+    if (!authHeader) return config
+    config.headers.Authorization = `Bearer ${authHeader}`
+    return config
+  })
   return (
     <BrowserRouter>
         <ToastContainer />
